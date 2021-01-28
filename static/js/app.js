b207ef6191ecc313597ee0df7ed1a7a0823ddc7c
JMazick <jazlynmm@outlook.com>
2021-01-27 19:07:45 -0600

    update app

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,24 @@
-// from data.js
-var tableData = data;
+// from samples.js
+d3.json("../data/samples.json").then(function(data) {
+    var metadata = data.metadata;
+// TO DO - come back and inputValue for id. nameplace will be part of a function for drop down
+    var filterMetadata = metadata.filter(demo => demo.id === nameplace);
+    var firstElement = filterMetadata[0];
+    var sampleMetadata = d3.select("#sample-metadata");
+    sampleMetadata.html("");
+
+Object.entries(firstElement).forEach(([key, value]) => sampleMetadata.append("h5").text(`${key}:${value}`));
+
+
+
+    
+    console.log(firstElement);
+  });
+
+
+
+  
+
+
 
 // YOUR CODE HERE!
