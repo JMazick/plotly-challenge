9112a7aedb8e968e2b073436b7795be42845f6a7
JMazick <jazlynmm@outlook.com>
2021-02-02 20:34:01 -0600

    step 2

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,15 @@
-// from samples.js
+// 1. Use the D3 library to read from samples.js
 d3.json("../data/samples.json").then(function(data) {
     var metadata = data.metadata;
+    
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+var samplesData = data.samples;
+var otuIDs = samplesData.filter(otuBar => otuBar.otu_ids === otuName);
+
+
+
+
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page
 // TO DO - come back and inputValue for id. nameplace will be part of a function for drop down
     var filterMetadata = metadata.filter(demo => demo.id === nameplace);
     var firstElement = filterMetadata[0];
