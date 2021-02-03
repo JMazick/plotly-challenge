feceeb02cafe156c7843a36299172b6c767f9084
JMazick <jazlynmm@outlook.com>
2021-02-02 20:48:00 -0600

    filter otu

--- static/js/app.js
+++ static/js/app.js
@@ -4,24 +4,31 @@ d3.json("../data/samples.json").then(function(data) {
     
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 var samplesData = data.samples;
-var otuIDs = samplesData.filter(otuBar => otuBar.otu_ids === otuName);
+// Create a custom function to return players who made the team
+function otuIDs (OTUs) {
+    return OTUs.otu_ids;
+  }
+// Call the custom function with filter()
+var otuID = samplesData.filter(otuIDs);
 
+
+  
 
 
 
 // 5. Display each key-value pair from the metadata JSON object somewhere on the page
 // TO DO - come back and inputValue for id. nameplace will be part of a function for drop down
-    var filterMetadata = metadata.filter(demo => demo.id === nameplace);
-    var firstElement = filterMetadata[0];
-    var sampleMetadata = d3.select("#sample-metadata");
-    sampleMetadata.html("");
+//     var filterMetadata = metadata.filter(demo => demo.id === nameplace);
+//     var firstElement = filterMetadata[0];
+//     var sampleMetadata = d3.select("#sample-metadata");
+//     sampleMetadata.html("");
 
-Object.entries(firstElement).forEach(([key, value]) => sampleMetadata.append("h5").text(`${key}:${value}`));
+// Object.entries(firstElement).forEach(([key, value]) => sampleMetadata.append("h5").text(`${key}:${value}`));
 
 
 
     
-    console.log(firstElement);
+    console.log(otuID);
   });
 
 
